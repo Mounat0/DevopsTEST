9d3cf32912108c7fd5b701101b72fc2394d9cd85
Mounat0 <53080323+Mounat0@users.noreply.github.com>
2019-09-05 10:26:03 +0100

    Update Login.java

--- devopstest/src/mounat/devops/Login.java
+++ devopstest/src/mounat/devops/Login.java
@@ -25,22 +25,14 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    
-	public void init() throws ServletException {
-		//we can create DB connection resource here and set it to Servlet context
-		if(getServletContext().getInitParameter("dbURL").equals("jdbc:mysql://localhost/mysql_db") &&
-				getServletContext().getInitParameter("dbUser").equals("mysql_user") &&
-				getServletContext().getInitParameter("dbUserPwd").equals("mysql_pwd"))
-		getServletContext().setAttribute("DB_Success", "True");
-		else throw new ServletException("DB Connection error");
-	}
+ 
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		//get request parameters for userID and password
-		String user = request.getParameter("user");
-		String pwd = request.getParameter("pwd");
+		String user = "munat";
+		String pwd = "123456" ;
 		
 		//get servlet config init params
 		String userID = getServletConfig().getInitParameter("user");
@@ -49,13 +41,7 @@ public class LoginServlet extends HttpServlet {
 		log("User="+user+"::password="+pwd);
 		
 		if(userID.equals(user) && password.equals(pwd)){
-			response.sendRedirect("LoginSuccess.jsp");
-		}else{
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-			PrintWriter out= response.getWriter();
-			out.println("<font color=red>Either user name or password is wrong.</font>");
-			rd.include(request, response);
-			
+			system.out.println(" Hi Munat ");
 		}
 		
 	}
