c81d9b942cc75f3e7cc36c7cdb1a9344dde7d1cd
Mounat0 <53080323+Mounat0@users.noreply.github.com>
2019-09-05 10:26:30 +0100

    update

--- devopstest/src/mounat/devops/Login.java
+++ devopstest/src/mounat/devops/Login.java
@@ -19,8 +19,8 @@ import javax.servlet.http.HttpServletResponse;
 		description = "Login Servlet", 
 		urlPatterns = { "/LoginServlet" }, 
 		initParams = { 
-				@WebInitParam(name = "user", value = "Pankaj"), 
-				@WebInitParam(name = "password", value = "journaldev")
+				@WebInitParam(name = "user", value = "null"), 
+				@WebInitParam(name = "password", value = "null")
 		})
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
